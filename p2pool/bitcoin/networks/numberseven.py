293403d73aa994db371687e025ed8d136015ebe3
pembo210 <pembo210@users.noreply.github.com>
2016-02-16 11:20:50 -0500

    block period

--- p2pool/bitcoin/networks/numberseven.py
+++ p2pool/bitcoin/networks/numberseven.py
@@ -18,7 +18,7 @@ RPC_CHECK = defer.inlineCallbacks(lambda bitcoind: defer.returnValue(
         ))
 SUBSIDY_FUNC = lambda height: 300*100000000 >> (height + 1)//420000
 POW_FUNC = lambda data: pack.IntType(256).unpack(__import__('ltc_scrypt').getPoWHash(data))
-BLOCK_PERIOD = 150 # s
+BLOCK_PERIOD = 420 # s
 SYMBOL = 'N7'
 CONF_FILE_FUNC = lambda: os.path.join(os.path.join(os.environ['APPDATA'], 'Number7') if platform.system() == 'Windows' else os.path.expanduser('~/Library/Application Support/number7/') if platform.system() == 'Darwin' else os.path.expanduser('~/.number7'), 'number7.conf')
 BLOCK_EXPLORER_URL_PREFIX = 'http://moonblocks.com:7000/block/'
