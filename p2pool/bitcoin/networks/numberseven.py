fe5e56db896e4c571dfff9334214f97120242c63
pembo210 <pembo210@users.noreply.github.com>
2016-02-16 03:42:25 -0500

    block explorer

--- p2pool/bitcoin/networks/numberseven.py
+++ p2pool/bitcoin/networks/numberseven.py
@@ -21,9 +21,9 @@ POW_FUNC = lambda data: pack.IntType(256).unpack(__import__('ltc_scrypt').getPoW
 BLOCK_PERIOD = 150 # s
 SYMBOL = 'N7'
 CONF_FILE_FUNC = lambda: os.path.join(os.path.join(os.environ['APPDATA'], 'Number7') if platform.system() == 'Windows' else os.path.expanduser('~/Library/Application Support/number7/') if platform.system() == 'Darwin' else os.path.expanduser('~/.number7'), 'number7.conf')
-BLOCK_EXPLORER_URL_PREFIX = 'http://explorer.litecoin.net/block/'
-ADDRESS_EXPLORER_URL_PREFIX = 'http://explorer.litecoin.net/address/'
-TX_EXPLORER_URL_PREFIX = 'http://explorer.litecoin.net/tx/'
+BLOCK_EXPLORER_URL_PREFIX = 'http://moonblocks.com:7000/block/'
+ADDRESS_EXPLORER_URL_PREFIX = 'http://moonblocks.com:7000/address/'
+TX_EXPLORER_URL_PREFIX = 'http://moonblocks.com:7000/tx/'
 SANE_TARGET_RANGE = (2**256//1000000000 - 1, 2**256//1000 - 1)
 DUMB_SCRYPT_DIFF = 2**16
 DUST_THRESHOLD = 0.03e8
